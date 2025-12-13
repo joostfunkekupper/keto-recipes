89cdee692d60fb16399661d0b35b20486ab44fc3
Joost Funke Kupper <joostfunkekupper@gmail.com>
2025-12-13 12:47:06 +1100

    Show which recipes are marked as public

--- components/Recipes.tsx
+++ components/Recipes.tsx
@@ -896,9 +896,9 @@ export default function Recipes({ recipeType }: RecipesProps) {
                   <h3 className="text-xl font-semibold text-gray-800">
                     {recipe.name}
                   </h3>
-                  {session?.user?.id === recipe.createdById && (
-                    <span className="inline-block px-3 py-1 text-xs font-medium text-green-700 bg-green-100 rounded-full">
-                      My recipe
+                  {session?.user?.id === recipe.createdById && recipe.isPublic && (
+                    <span className="inline-block px-3 py-1 text-xs font-medium text-blue-700 bg-blue-100 rounded-full">
+                      Public
                     </span>
                   )}
                 </div>
